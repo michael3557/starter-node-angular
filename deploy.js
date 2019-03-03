add62041f983185403a041b33d9b88e1a701cb1c
Michael Davis <michaeld3557@gmail.com>
2019-03-03 14:34:41 -0700

    deploy.js added mkdir for mkdir Starter-Node-Angular

--- deploy.js
+++ deploy.js
@@ -22,7 +22,7 @@ function installPM2() {
 // transfers local project to the remote server
 function transferProjectToRemote(failed, successful) {
   return ssh.putDirectory(
-    '../Starter-Node-Angular',
+    '../starter-node-angular',
     '/home/ubuntu/Starter-Node-Angular-temp',
     {
       recursive: true,
